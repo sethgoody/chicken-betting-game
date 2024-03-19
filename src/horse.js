7e0f70480881896d83c46ac6de9093a023aa0726
Sethgood7 <sethgooodman657@gmail.com>
2024-03-18 22:15:51 -0700

    got horse to move

--- src/horse.js
+++ src/horse.js
@@ -1,3 +1,4 @@
+// creating images on the dom
 function newImage(url){
     let image = document.createElement('img')
     image.src = url
@@ -5,7 +6,7 @@ function newImage(url){
     document.body.append(image)
     return image
 }
-
+// Generating Bojack
 function BoJack(x, y) {
     const element = newImage('./assets/BoJack/tile015.png')
     element.style.zIndex = 2;
@@ -13,6 +14,17 @@ function BoJack(x, y) {
 
     element.style.left = `${x}px`;
     element.style.top = `${y}px`;
+
+    function changeDirection(direction) {
+        if (direction === null) {
+            element.src = `./assets/BoJack/tile015.png`
+        }
+        if (direction === 'east') {
+            element.src = `./assets/BoJack/run.gif`
+        }
+    }
+    move(element).startRace(x, y, changeDirection)
+
     
     return {
         element: element,
@@ -20,4 +32,55 @@ function BoJack(x, y) {
     }
 }
 
+// placing all horses
 let horse1 = BoJack(150, 250)
+
+// creating movement
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function startRace(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveAllObjects(){ 
+            if(direction === 'east'){
+                x+=1
+            }
+
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveAllObjects, 1)
+
+        // creating controls
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowUp'){
+                direction = 'east'
+            }
+            callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = 'east'
+            callback(direction)
+        })
+    }
+
+    return {
+        to: moveToCoordinates,
+        startRace: startRace
+    }
+}
