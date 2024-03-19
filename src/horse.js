b5d02e4ceeb5289e14dc8d084890a4701442d6ce
Esteban Sepulveda <tebansd@icloud.com>
2024-03-19 11:23:45 -0700

    added all horses

--- src/horse.js
+++ src/horse.js
@@ -1,54 +1,53 @@
-// Creating images on the DOM
+// Create and append horses images to the DOM
 function newImage(url, x, y) {
-  let image = document.createElement("img");
-  image.src = url;
-  image.style.position = "absolute";
-  image.style.left = `${x}px`;
-  image.style.top = `${y}px`;
-  document.body.append(image);
-  return image;
-}
-
-// Generating Bojack with a function to start racing
-function BoJack(x, y) {
-  const runGif = "./assets/BoJack/run.gif";
-  const standImg = "./assets/BoJack/tile015.png";
-  const element = newImage(standImg, x, y);
-
-  function startRace() {
-    element.src = runGif; // Change to running gif when race starts
-    let currentPosition = x;
-
-    const interval = setInterval(() => {
-      currentPosition += 10; // Adjust the speed if need be
-      element.style.left = `${currentPosition}px`;
-
-      // This stops the horse at the end of the viewport - can change to fit track
-      if (currentPosition > window.innerWidth - 100) {
-        clearInterval(interval);
-        element.src = standImg;
-      }
-    }, 50); // Adjust the interval as needed for smoother animation
+    let image = document.createElement("img");
+    image.src = url;
+    image.style.position = "absolute";
+    image.style.left = `${x}px`;
+    image.style.top = `${y}px`;
+    document.body.append(image);
+    return image;
   }
+  
+  // Function to create a horse object with a name and starting position, and a method to start its race
+  function createHorse(name, x, y) {
+    const runGif = `./assets/${name}/run.gif`;
+    const standImg = `./assets/${name}/tile015.png`;
+    const element = newImage(standImg, x, y);
+  
+    function startRace() {
+      element.src = runGif;
+      let currentPosition = x;
+  
+      const interval = setInterval(() => {
+        currentPosition += 10; // Moves horse 10px to the right ----- adjust if need
+        element.style.left = `${currentPosition}px`;
+  
+        if (currentPosition > window.innerWidth - 100) {
+          clearInterval(interval);
+          element.src = standImg;
+        }
+      }, 50);
+    }
+  
+    return { startRace };
+  }
+  
+  // Initialize multiple horses
+  let horses = [
+    createHorse("BoJack", 150, 250),
+    createHorse("Copenhagen", 150, 350),
+    createHorse("Bullseye", 150, 450),
+    createHorse("Pegasus", 150, 550),
+    createHorse("Rusty", 150, 650)
 
-  return {
-    startRace: startRace,
-  };
-}
-
-// Placing BoJack on the track
-let horse1 = [
-    BoJack(150, 250),
-    Bullseye()
-]
-
-// Links start race button with the start race function
-document.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelector(".start-button")
-    .addEventListener("click", function () {
-      horse1.startRace();
-
-      document.querySelector(".start-button").style.display = "none";
+  ];
+  
+  // Start races and hide button on click
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".start-button").addEventListener("click", function () {
+      horses.forEach(horse => horse.startRace());
+      this.style.display = "none";
     });
-});
+  });
+  
